6ad1ef6cc9d2a5e4d95eaa0b4cf6c39cb0539720
Daniel <daniel.minior.pk@gmail.com>
2025-04-27 14:38:21 +0200

    Init and Groovy config

---
+++ common/src/test/java/pl/dminior8/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dminior8.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-service/src/test/java/pl/dminior8/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dminior8.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
